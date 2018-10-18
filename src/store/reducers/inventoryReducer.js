a0967756dda0d4c394bfe5b0648fbb25b13cf7cc
jasonjalufka <jasonjalufka@gmail.com>
2018-10-18 13:06:08 -0500

    Update inventoryReducer to return default state

--- src/store/reducers/inventoryReducer.js
+++ src/store/reducers/inventoryReducer.js
@@ -2,17 +2,14 @@ import * as actionTypes from '../actions';
 import drinks from '../../data/drinks';
 
 const initialState = {
-    milk: 200
+  milk: 200
 };
 
 const reducer = (state = initialState, action) => {
-    // switch(action.type)
-    // case 
-    // return {
-    //     ...state,
-    //     //TODO CHANGE 
-    //     milk: state.milk 
-    // }
-}
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
 
 export default reducer;
