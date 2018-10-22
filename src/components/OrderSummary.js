5f59b1845d9bc688dbc5625eea20297fc69634a5
abigailbarron <abbyeb22@gmail.com>
2018-10-22 13:18:01 -0500

    Add calculated cost per order item

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Card from '@material-ui/core/Card';
+import drinks from "../data/drinks";
+import milkCost from "../data/milkCost";
 
 const OrderSummary = (props) =>  {
 const {order} = props;
 let orderCountIndex = Object.keys(order).length-1;
+
+let getTotal = orderKey => {
+    let itemCost = 0;
+    itemCost = drinks[order[orderKey].drinkOption].price + milkCost[order[orderKey].milkOption];
+    return itemCost;
+}
 return(
     <Card>
         {
-           
             Object.keys(order).map((orderKey, index) => (
                 <div key={index}>
                 {
@@ -17,7 +24,9 @@ return(
                         <ListItemText primary={order[orderKey].drinkOption}/>
                         <ListItemText primary={order[orderKey].beanOption}/>
                         <ListItemText primary={order[orderKey].milkOption}/>
-                        </ListItem>
+                        <ListItemText primary={'$' + getTotal(orderKey)}/>
+                    </ListItem>
+                        
                 }
                 </div>
             ))
