bf82cd19fee01d60c50fbd3d57f2e7c8dd1803ab
abigailbarron <abbyeb22@gmail.com>
2018-10-27 15:21:41 -0500

    Update field to TextField for drinks and price

--- src/components/MenuConfig.js
+++ src/components/MenuConfig.js
@@ -11,13 +11,29 @@ import Button from '@material-ui/core/Button';
 import inventory from '../data/inventory';
 import drinks from '../data/drinks';
 import milkCost from "../data/milkCost";
+import { TextField } from '@material-ui/core';
 
+const renderTextField = ({
+    input,
+    defaultValue,
+  }) => (
+    <TextField
+    defaultValue={defaultValue}
+    onChange={input.onChange}
+    />
+  )
 
-let MenuConfig =(props) => {
+  let handleChange = e => {
+      console.log("in handleChange");
+      return e.target.value;
+  }
+
+let MenuConfig = props => {
     const {handleSubmit} = props;
     const submitForm = (formValues) => {
         props.onSubmit(formValues);
         console.log("in submitForm of MenuConfig");
+        console.log(formValues);
         props.reset();
     }
     return (
@@ -31,11 +47,11 @@ let MenuConfig =(props) => {
                         <List subheader={<ListSubheader component="div">Drinks</ListSubheader>}>
                             {
                                 Object.keys(drinks).map((drink, index) => (
-                                    <Grid container space={8}>
+                                    <Grid container space={8} key={index}>
                                         <Grid item xs>
                                             <ListItem>
-                                                <Field name={drink} placeholder={drink} component="input" type="text" value={drink} />
-                                                <Field label="$" name={drink + "Price"} placeholder={drinks[drink].price} component="input" type="text" value={""+drinks[drink].price} />
+                                                <Field name={drink} onChange={handleChange} defaultValue={drink} component={renderTextField}/>
+                                                <Field name={drink + "Price"} onChange={handleChange} defaultValue={drinks[drink].price} component={renderTextField} />
 
                                                 {/* <ListItemText primary={drink} /> */}
                                             </ ListItem>
