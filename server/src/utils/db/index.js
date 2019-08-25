75a857fc88e155baab09340eb246cfaf1c052381
Jason Jalufka <jasonjalufka@gmail.com>
2019-08-25 16:07:53 -0700

    Update db to connect to mongodb cloud

--- server/src/utils/db/index.js
+++ server/src/utils/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const connection = mongoose.connect("mongodb://localhost:27017/kervTest");
+const connection = mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`, { useNewUrlParser: true });
 const Populate = require('../populateDB');
 connection
     .then(db => {
